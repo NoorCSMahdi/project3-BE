3230add87c220ba36b0a71aafe5daec68683c8ea
marwaa <mar.alkhashram@gmail.com>
2023-12-28 17:05:53 +0300

    Server routes

--- server.js
+++ server.js
@@ -18,12 +18,19 @@ const db = require("./config/db");
 //Import Routes
 const indexRouter = require("./routes/index");
 const userRouter = require("./routes/user");
+const carRouter = require("./routes/car");
+const cartypeRouter = require("./routes/cartype");
+const authRouter = require("./routes/auth");
 
 //telling Nodejs to look into the folder called views for all the ejs files
 app.set("view engine", "ejs");
 
 //Mount Routes
+app.use('/', indexRouter);
 app.use("/user", userRouter);
+app.use("/car", carRouter);
+app.use("/cartype", cartypeRouter);
+app.use('/auth', authRouter);
 
 app.listen(port, () => {
     console.log(`Voiture App is running on port ${port}`);
